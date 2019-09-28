1c9a46bd45f1f06588eb672c359cea59b6618c49
Nathan Murray <nathanmurray@NMURRAY-MBP-DC.local>
2019-09-28 12:30:21 -0400

    works except for save

--- training/inference.py
+++ training/inference.py
@@ -25,7 +25,7 @@ base_dir = args.base_dir
 IMAGE_SIZE = 224
 BATCH_SIZE = 64
 
-#df=pandas.read_csv(args.labels, names=('file','sfw_score','nsfw_score'))
+df=pandas.read_csv(args.labels, names=('file','sfw_score','nsfw_score'))
 
 #to reduce testing size
 #if args.sample_size is not None:
@@ -34,7 +34,7 @@ BATCH_SIZE = 64
 datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255, 
     validation_split=0.2)
-"""
+
 train_generator=datagen.flow_from_dataframe(
     directory=base_dir,
     dataframe=df,
@@ -53,8 +53,8 @@ val_generator=datagen.flow_from_dataframe(
     target_size=(IMAGE_SIZE, IMAGE_SIZE),
     batch_size=BATCH_SIZE,
     subset='validation')
-"""
 
+"""
 train_generator = datagen.flow_from_directory(
     base_dir,
     target_size=(IMAGE_SIZE, IMAGE_SIZE),
@@ -66,7 +66,7 @@ val_generator = datagen.flow_from_directory(
     target_size=(IMAGE_SIZE, IMAGE_SIZE),
     batch_size=BATCH_SIZE, 
     subset='validation')
-
+"""
 IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
 
 base_model = tf.keras.applications.MobileNetV2(
@@ -86,44 +86,47 @@ model = tf.keras.Sequential([
   tf.keras.layers.Conv2D(32, 3, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.GlobalAveragePooling2D(),
-  tf.keras.layers.Dense(2, activation='softmax')
+  tf.keras.layers.Dense(1)
 ])
 
 model.compile(optimizer=tf.keras.optimizers.Adam(), #TODO check if this is correct
-              loss='categorical_crossentropy', 
-              metrics=['accuracy', 'binary_accuracy', 'mean_absolute_error', 'mean_absolute_percentage_error', 'cosine_proximity'])
+              loss='mean_absolute_error', 
+              metrics=['accuracy'])
 
 print(model.summary())
 
 print('Number of trainable variables = {}'.format(len(model.trainable_variables)))
 
-epochs = 10
+epochs = 1
 
 history = model.fit_generator(train_generator, 
                     epochs=epochs, 
                     validation_data=val_generator)
+print(model.summary())
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+saved_model_dir = 'training/models'
+tf.saved_model.save(model, saved_model_dir)
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+#converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+#tflite_model = converter.convert()
 
-plt.figure(figsize=(8, 8))
-plt.subplot(2, 1, 1)
-plt.plot(acc, label='Training Accuracy')
-plt.plot(val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
+#with open('model.tflite', 'wb') as f:
+#  f.write(tflite_model)
+
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
 plt.ylabel('Accuracy')
-plt.ylim([min(plt.ylim()),1])
-plt.title('Training and Validation Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
 
-plt.subplot(2, 1, 2)
-plt.plot(loss, label='Training Loss')
-plt.plot(val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.ylabel('Cross Entropy')
-plt.ylim([0,1.0])
-plt.title('Training and Validation Loss')
-plt.xlabel('epoch')
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
