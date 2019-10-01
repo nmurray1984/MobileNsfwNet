09f1d1cf0def2d34acd8f5a3cea28c732c0ff546
Nathan Murray <nathanmurray@NMURRAY-MBP-DC.local>
2019-10-01 19:33:53 -0400

    pull from azure

---
+++ training/azure_content_monitor.py
@@ -0,0 +1,40 @@
+import glob
+import argparse
+import os
+import urllib
+import requests
+import json
+
+parser = argparse.ArgumentParser('run images through azure and get labels to stdout')
+
+parser.add_argument('--base_dir',
+                    help='base directory for images')
+
+parser.add_argument('--subscription_key', 
+                    help='subscription key for api')
+
+args = parser.parse_args()
+
+files_search = os.path.join(args.base_dir, '*.jpg')
+files = glob.glob(files_search)
+
+headers = {
+    # Request headers
+    'Content-Type': 'image/jpeg',
+    'Ocp-Apim-Subscription-Key': args.subscription_key,
+}
+
+url = 'https://southcentralus.api.cognitive.microsoft.com/contentmoderator/moderate/v1.0/ProcessImage/Evaluate?CacheImage=false'
+
+for image in files:
+    with open('flower_photos/sunflowers/6204049536_1ac4f09232_n.jpg', 'rb') as f:
+        response = requests.post(url, headers=headers, data=f)
+        text = json.loads(response.text)
+        print('{},{},{},{},{},{}'.format(
+            image,
+            text['Status']['Description'],
+            text['IsImageRacyClassified'],
+            text['RacyClassificationScore'],
+            text['IsImageRacyClassified'],
+            text['AdultClassificationScore'],
+        ))
