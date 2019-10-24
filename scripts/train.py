f764db7600421bcd61fcad38741e834b1c830e26
Nathan Murray <nathanmurray@NMURRAY-MBP-DC.local>
2019-10-24 06:27:21 -0400

    updated training script

--- scripts/train.py
+++ scripts/train.py
@@ -1,15 +1,23 @@
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import StandardScaler
-from tensorflow.python.keras.utils import Sequence
-from keras import backend as K
-import matplotlib.pyplot as plt # plotting
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pandas
 import numpy as np # linear algebra
 import os # accessing directory structure
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import tensorflow as tf
 import os
 import glob
 
+import argparse
+
+parser = argparse.ArgumentParser('Train from a csv')
+
+parser.add_argument('--save_model_file',
+                    help='save_model_file')
+
+parser.add_argument('--csv_file',
+                    help='file with list of files and their classes')
+
+args = parser.parse_args()
 
 #ensures repeatability of experiments
 from numpy.random import seed
@@ -17,46 +25,46 @@ seed(1)
 from tensorflow import set_random_seed
 set_random_seed(2)
 
-class NumPyFileGenerator(Sequence):
-    def __init__(self, files):
-        self.files = files
+df=pandas.read_csv(args.csv_file)
 
-    def __len__(self):
-        return len(self.files)
+datagen = ImageDataGenerator(rescale=1./255)
 
-    def __getitem__(self, idx):
-        data = np.load(open(self.files[idx], 'rb'),allow_pickle=True)
-        x = data['MobileNetV2_bottleneck_features']
-        a = data['azure_output']
-        b = map(lambda x: 1 if x else 0, a[:,3].tolist())
-        y = list(b)
-        return x, y
+train_generator = datagen.flow_from_dataframe(
+        dataframe=df[0:39999],
+        directory='',
+        x_col="file_name",
+        y_col="new_class",
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical')
 
-all_files = glob.glob("/kaggle/input/*/*/*/*/*.npz")
-len(all_files)
+validation_generator = datagen.flow_from_dataframe(
+        dataframe=df[40000:47750],
+        directory='',
+        x_col="file_name",
+        y_col="new_class",
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical')
 
-training_generator = NumPyFileGenerator(files=all_files[0:2999])
-validation_generator = NumPyFileGenerator(files=all_files[3000:3475])
+base_model = tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3),
+                                              include_top=False, 
+                                              weights='imagenet')
+
+base_model.trainable = False
 
 model = tf.keras.Sequential([
+  base_model,
   tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(7, 7, 1280)),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.GlobalAveragePooling2D(),
-  tf.keras.layers.Dense(1,activation='sigmoid')
+  tf.keras.layers.Dense(3,activation='softmax')
 ])
+model.compile(optimizer=tf.keras.optimizers.Adam(), 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
 
-model.compile(optimizer='rmsprop',
-              loss='binary_crossentropy',
-              metrics=['accuracy', 
-                       tf.keras.metrics.FalsePositives(), 
-                       tf.keras.metrics.FalseNegatives(),
-                       binary_accuracy_50, 
-                       binary_accuracy_60, 
-                       binary_accuracy_70, 
-                       binary_accuracy_80, 
-                       binary_accuracy_90])
-
-history = model.fit_generator(training_generator, 
+history = model.fit_generator(train_generator, 
                     epochs=10, 
                     validation_data=validation_generator)
 
