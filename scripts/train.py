fb6e56c910517ec4a53ea436c6124867f2ae0412
Nathan Murray <nathanmurray@NMURRAY-MBP-DC.local>
2019-10-29 08:46:40 -0400

    updated train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.utils import Sequence
+from tensorflow.keras.utils import to_categorical
 import pandas
 import numpy as np # linear algebra
 import os # accessing directory structure
@@ -27,38 +29,62 @@ set_random_seed(2)
 
 df=pandas.read_csv(args.csv_file)
 
-datagen = ImageDataGenerator(rescale=1./255)
+class NumPyFileGenerator(Sequence):
+    def __init__(self, file_list, batch_size):
+        self.file_list = file_list
+        self.class_list = file_list['new_class'].tolist()
+        self.file_name_list = file_list['file_name'].tolist()
+        self.batch_size = batch_size
 
-train_generator = datagen.flow_from_dataframe(
-        dataframe=df[0:39999],
-        directory='',
-        x_col="file_name",
-        y_col="new_class",
-        target_size=(224, 224),
-        batch_size=32,
-        class_mode='categorical')
+    def __len__(self):
+        return int(len(self.file_name_list) / self.batch_size)
 
-validation_generator = datagen.flow_from_dataframe(
-        dataframe=df[40000:47750],
-        directory='',
-        x_col="file_name",
-        y_col="new_class",
-        target_size=(224, 224),
-        batch_size=32,
-        class_mode='categorical')
+    def get_bottleneck_file_name(self, original):
+        dirname = os.path.dirname(original)
+        basename = os.path.basename(original)
+        #hardcoding for now
+        model = 'mobilenet_v2_1.0_224'
+        new_basename = basename.replace('224x224.jpg', model + '-bottleneck.npz')
+        return os.path.join(dirname, new_basename)
 
-base_model = tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3),
-                                              include_top=False, 
-                                              weights='imagenet')
+    def __getitem__(self, idx):
+        x = np.empty((0, 7, 7, 1280))
+        y = np.empty((0))
+        for i in range(idx * self.batch_size, ((idx + 1) * self.batch_size)):
+                file_name = self.file_name_list[i]
+                npz_file = self.get_bottleneck_file_name(file_name)
+                bottleneck_data = np.load(npz_file, allow_pickle=True)
+                x_row = bottleneck_data['bottleneck']
+                x = np.append(x, [x_row], axis=0)
+                y_row = 0
+                if self.class_list[i] == 'sunflowers':
+                        y_row = 1
+                y = np.append(y, [y_row], axis=0)
+        return x, to_categorical(y, num_classes=2)
 
-base_model.trainable = False
+VALIDATION_PERCENT = .20
+
+train_start = 0
+train_end = int(len(df) * (1 - VALIDATION_PERCENT))
+validation_start = train_end + 1
+validation_end = len(df)
+print(train_end)
+print(validation_end)
+train_generator = NumPyFileGenerator(df[train_start:train_end], 32)
+validation_generator = NumPyFileGenerator(df[validation_start:validation_end], 32)
+
+#base_model = tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3),
+#                                              include_top=False, 
+#                                              weights='imagenet')
+
+#base_model.trainable = False
 
 model = tf.keras.Sequential([
-  base_model,
+  #base_model,
   tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(7, 7, 1280)),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.GlobalAveragePooling2D(),
-  tf.keras.layers.Dense(3,activation='softmax')
+  tf.keras.layers.Dense(2,activation='softmax')
 ])
 model.compile(optimizer=tf.keras.optimizers.Adam(), 
               loss='categorical_crossentropy', 
